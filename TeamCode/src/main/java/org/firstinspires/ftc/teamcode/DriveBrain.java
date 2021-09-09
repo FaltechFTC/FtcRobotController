0f88269e5ae76270ef30695a191069e5f071228a
Doof6473 <rishi.yarlagadda10@gmail.com>
2021-09-09 16:51:27 -0500

    RGBA done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBrain.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
@@ -64,10 +65,31 @@ public class DriveBrain {
     public void gyroTurn() {
 
     }
-    public void color() {
-
+    public boolean checkWhite(NormalizedRGBA color) {
+        if(color.blue > 200 && color.red > 200  && color.green >200)
+            return true;
+        return false;
     }
-    public void driveToColor() {
 
-    }
+    public void driveToWhite(double power, double timeoutSeconds) {
+        runtime.reset();
+
+        double forward = power;
+
+//  TODO      if (opmode.opModeIsActive()) {
+        robot.setDrive(forward, 0, 0, power);
+
+        NormalizedRGBA rgba = robot.getRGBA();
+        boolean white = checkWhite(rgba);
+
+        while (/*TODO  opModeIsActive() && */(runtime.seconds() < timeoutSeconds) && !white) {
+            rgba = robot.getRGBA();
+            white = checkWhite(rgba);
+
+            // Display it for the driver.
+            opmode.telemetry.addData("white",  "%b", white);
+            opmode.telemetry.update();
+        }
+        robot.setDriveStop();
+        robot.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
 }
