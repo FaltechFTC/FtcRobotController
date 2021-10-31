f944eb3525d721a855f87031e7a24a12407784ac
DoG <59429045+DogISm@users.noreply.github.com>
2021-10-31 13:26:28 -0500

    Commit message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xdrive/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xdrive/Auto.java
@@ -55,49 +55,38 @@ public class Auto extends LinearOpMode {
         if (side) angleModifier=-1;
         telemetry.addData("Starting!",0);
         //deliver preloaded box
-        driveBrain.driveDistance(1,.3, 1);//move forward 1in
-
-        driveBrain.rotateToHeadingAbsolute(-30,2,0.5,3);
-
-        driveBrain.driveDistance(34,.5,2);//drives to shipping hub
+        driveBrain.driveDistance(1,mediumPower, shortTimeout);//move forward 1in
+        driveBrain.rotateToHeadingAbsolute(-30*angleModifier,2,0.5,mediumTimeout);
+        driveBrain.driveDistance(34,halfPower,mediumTimeout);//drives to shipping hub
         telemetry.addData("Moved to shipping hub!",1);
         //TODO move the arm so that we can place the game piece
         telemetry.addData("Placed Block!",2);
         //gets duck off of carousel
-        driveBrain.driveDistance(1,.5,1);//drives 1 inch in order to get enough room to spin
-
-        driveBrain.rotateToHeadingAbsolute(140,3,0.5,3);//rotates so that it is facing carousel
-
-        driveBrain.driveDistance(68, .75, 3);//drives to carousel
-
+        driveBrain.driveDistance(1,halfPower,shortTimeout);//drives 1 inch in order to get enough room to spin
+        driveBrain.rotateToHeadingAbsolute(110*angleModifier,3,halfPower,mediumTimeout);//rotates so that it is facing carousel
+        driveBrain.driveDistance(68, highPower, mediumTimeout);//drives to carousel
         driveBrain.carouselMoves();//moves the carousel wheel
         telemetry.addData("Moved the Carousel",3);
-
         //Gets a block from the warehouse and delivers it
-        driveBrain.driveDistance(-1, .3, 1);//goes back so that it has room to turn
-
-        driveBrain.rotateToHeadingAbsolute(160,2,.3,5);
-
-        driveBrain.driveDistance(96,.5, 6);//drives to warehouse
+        driveBrain.driveDistance(-1, mediumTimeout, shortTimeout);//goes back so that it has room to turn
+        driveBrain.rotateToHeadingAbsolute(270*angleModifier,2,mediumPower,highTimeout);
+        driveBrain.driveDistance(96,halfPower, highTimeout);//drives to warehouse
         telemetry.addData("Drove to Warehouse!",4);
         //TODO make it so that the arm and claw work together to get a block.
-        driveBrain.driveDistance(-72,.5,5);
+        driveBrain.driveDistance(-72,halfPower,highTimeout);
 
-        driveBrain.rotateToHeadingAbsolute(90,2,.5,5);
-
-        driveBrain.driveDistance(33, .6,5);//drives to shipping hub
+        driveBrain.rotateToHeadingAbsolute(360*angleModifier,2,halfPower,highTimeout);
+        driveBrain.driveDistance(33, highPower,highTimeout);//drives to shipping hub
         telemetry.addData("Drove to Shipping Hub!",5);
         //TODO places block down
         telemetry.addData("Placed Block!",6);
         //Parks in the warehouse
-        driveBrain.driveDistance(-33, .2, 1);//goes back so that it has enough room to turn
-
-        driveBrain.rotateToHeadingAbsolute(-90,2,.5,2);
-
-        driveBrain.driveDistance(72, .5, 3);//drives to go inside the warehouse
+        driveBrain.driveDistance(-33, slowPower, shortTimeout);//goes back so that it has enough room to turn
+        driveBrain.rotateToHeadingAbsolute(270*angleModifier,2,halfPower,mediumTimeout);
+        driveBrain.driveDistance(72, halfPower, mediumTimeout);//drives to go inside the warehouse
         telemetry.addData("Drove to Warehouse!",7);
         //end
-        telemetry.addData("Autonomous Complete!",8);
+        telemetry.addData("Autonomous For Resource Depot Complete!",8);
     }
     public void redAutoWarehouse(boolean side) {
         int barcode = 0; //TODO get barcode number from vision
