85cafa8efc22eb84e7c8001f897af3692af1f8f7
Jackson Taylor <jacksondeantaylor@gmail.com>
2021-11-06 17:26:13 -0500

    rewrote tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xdrive/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xdrive/Tele.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.Utility;
 public class Tele extends OpMode {
     Robot robot = new Robot();
     DriveBrain driveBrain;
-   double fixedHeading = 0;
+    double fixedHeading = 0;
     double armOffset = 0.0;
     final double ARM_SPEED = 0.05;
     double armPos = 0;
@@ -19,6 +19,8 @@ public class Tele extends OpMode {
     LynxModule.BulkCachingMode readMode;
     ElapsedTime timer;
     int cycles;
+
+    static final double DRIVE_OVERDRIVE_MULTIPLE = 2;
 
     @Override
     public void init() {
@@ -32,157 +34,118 @@ public class Tele extends OpMode {
 
     @Override
     public void loop() {
-        boolean T_Mode = false;
-        double cPower = Utility.deadStick(gamepad2.right_stick_x);
-        Utility.deadStick(gamepad1.left_stick_x);
-        Utility.deadStick(gamepad1.left_stick_y);
-        Utility.deadStick(gamepad1.right_stick_x);
-        double forward = -0.5 * gamepad1.left_stick_y;
-        double strafe = 0.5 * gamepad1.left_stick_x;
-        double rotate = 0.25 * -gamepad1.right_stick_x;
-        if(gamepad1.right_bumper&&gamepad1.left_bumper){
-            if(gamepad1.a)Robot.useCarousel = true;
-            if(gamepad1.b)Robot.useCarousel = false;
-//            if(gamepad2.a)Robot.useArm = true;
-//            if(gamepad2.b)Robot.useArm = false;
-        }
-        if (gamepad1.left_bumper) {
-            if (gamepad1.a) {
-                robot.setBulkReadMode(LynxModule.BulkCachingMode.AUTO);
-                telemetry.addData("BReadMode: ", "AUTO");
-                timer.reset();
-                cycles = 0;
-            }
-            if (gamepad1.b) {
-                robot.setBulkReadMode(LynxModule.BulkCachingMode.OFF);
-                telemetry.addData("BReadMode: ", "OFF");
-                timer.reset();
-                cycles = 0;
-            }
-            if (gamepad1.x) {
-                robot.setBulkReadMode(LynxModule.BulkCachingMode.MANUAL);
-                telemetry.addData("BReadMode: ", "MANUAL");
-                timer.reset();
-                cycles = 0;
-            }
-            if (gamepad2.left_trigger > 0.0) {
-                robot.intakePusher.setPosition(gamepad2.left_trigger);
-                telemetry.addData("Pusher", gamepad2.left_trigger);
-            }
-//            if (gamepad2.right_trigger > 0.0) {
-//                robot.intakeWrist.setPosition(gamepad2.right_trigger);
-//                telemetry.addData("Wrist", gamepad2.right_trigger);
-//            }
+        double carousel_power = Utility.deadStick(gamepad2.right_stick_x);
+        double pusher_pos = gamepad2.left_trigger;
+        double arm_power = -2.0 * Utility.deadStick(gamepad2.left_stick_y);
+        if (arm_power > 0) arm_power *= 2;
+        double drive_forward = -0.5 * Utility.deadStick(gamepad1.left_stick_y);
+        double drive_strafe = 0.5 * Utility.deadStick(gamepad1.left_stick_x);
+        double drive_rotate = 0.25 * -Utility.deadStick(gamepad1.right_stick_x);
 
+        boolean drive_heading_lock = gamepad1.left_trigger > 0.05;
+        boolean drive_tmode = gamepad1.right_trigger > 0.05;
+        boolean drive_overdrive = gamepad1.right_bumper || gamepad2.right_bumper;
+        boolean pusher_cycle = gamepad1.left_bumper || gamepad1.a || gamepad2.left_bumper;
+        boolean arm_park = gamepad1.y || gamepad2.y;
+        boolean arm_layer1 = gamepad1.x || gamepad2.x;
+        boolean arm_intake = gamepad1.b || gamepad2.b;
+        boolean carousel_cycle = gamepad1.dpad_right || gamepad2.dpad_right;
+        boolean drive_rotate_90 = gamepad1.dpad_left || gamepad2.dpad_left;
+
+
+        if (pusher_cycle) {
+            robot.pusherClose();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                ; // eat it
+            }
+            robot.pusherOpen();
+        }
+        pusher_pos = Utility.clipToRange(pusher_pos, 1, 0);
+        robot.intakePusher.setPosition(pusher_pos);
+        if (drive_rotate_90) {
+            driveBrain.rotateToHeadingRelative(90, 3, 0.3, 3);
+            return;
+        }
+        if (Robot.useArm) {
+            if (arm_park) {
+                armPos = robot.ARM_PARK_POS;
+            }
+            else if (arm_layer1) {
+                armPos = robot.ARM_LAYER1_POS;
+            }
+            else if (arm_intake) {
+                armPos = robot.ARM_INTAKE_POS;
+            }
+            else
+                armPos += arm_power;
+            armPos = Utility.clipToRange(armPos, 1000, 0);
+            robot.setArmMotorPosition(armPos);
+        }
+        if (drive_overdrive) {
+            drive_forward *= DRIVE_OVERDRIVE_MULTIPLE;
+            drive_strafe *= DRIVE_OVERDRIVE_MULTIPLE;
+            drive_rotate *= DRIVE_OVERDRIVE_MULTIPLE;
+        }
+
+        if (drive_tmode) {
+            //calculate drive direction
+            double direction = Math.atan2(drive_forward,drive_strafe);
+            //offset by 45 degrees
+            direction += Math.toRadians(45);
+            //calculate back to strafe and forward
+            drive_forward = Math.sin(direction);
+            drive_strafe = Math.cos(direction);
+        }
+
+
+        double currentHeading = robot.getHeading(AngleUnit.DEGREES);
+        telemetry.addData("Our Heading", currentHeading);
+        if (!drive_heading_lock) {
+            fixedHeading = currentHeading;
         }
         else {
-            if (gamepad1.dpad_up) {
-                robot.setDriveStopModeBreak();
+            double headingError = Utility.wrapDegrees360(fixedHeading - currentHeading);
+            if (headingError > 45) {
+                headingError = 45;
             }
-            if (gamepad1.dpad_down) {
-                robot.setDriveStopModeFloat();
+            if (headingError < -45) {
+                headingError = -45;
             }
-            if (gamepad2.dpad_up) {
-                robot.pusherOpen();
-            }
-            if (gamepad2.dpad_down) {
-                robot.pusherClose();
-            }
-            if (gamepad1.x) {
-                driveBrain.rotateToHeadingRelative(90, 3, 0.3, 3);
-                return;
-            }
-            if (Robot.useArm) {
-                if (gamepad2.a) {
-                    armPos = 128;
-                }
-                else if (gamepad2.b) {
-                    armPos = 224;
-                }
-                else if (gamepad2.x) {
-                    armPos = 353;
-                }
-                //lvl 3 is 513
-                else
-                    armPos += gamepad2.left_stick_y * 2;
-                armPos = Utility.clipToRange(armPos, 1000, 0);
-                robot.setArmMotorPosition(armPos);
-            }
-            if (gamepad1.right_trigger == 0.00) {
-                telemetry.addData("T-Mode", T_Mode);
-            }
-            if (gamepad1.right_trigger > 0.00) {
-
-                T_Mode = true;
-                telemetry.addData("T-Mode", T_Mode);
-                if (Math.abs(strafe) > Math.abs(forward)) {
-                    forward = 0;
-                    // because we are strafing we can kill the forward
-                    //  strafe = strafe;
-                } else {
-                    strafe = 0;
-                    // because we are going forward, we will kill the strafe
-                    // forward = forward;
-                }
-                robot.setTwoWheelDrive(forward, strafe, rotate);
-                return;
-            }
-            if (gamepad1.right_bumper) {
-                forward = forward * 2;
-                strafe = strafe * 2;
-                rotate = rotate * 2;
-            }
-            double currentHeading = robot.getHeading(AngleUnit.DEGREES);
-            telemetry.addData("Our Heading", currentHeading);
-            if (gamepad1.left_trigger == 0.00) {
-                fixedHeading = currentHeading;
-                if (gamepad1.a && gamepad1.left_trigger == 0.00) {
-                    fixedHeading += 180;
-                }
-            }
-            if (gamepad1.left_trigger > 0.00) {
-
-                double headingError = Utility.wrapDegrees360(currentHeading - fixedHeading);
-                if (headingError > 45) {
-                    headingError = 45;
-                }
-                if (headingError < -45) {
-                    headingError = -45;
-                }
-                if (Math.abs(headingError) < .5) {
-                    headingError = 0;
-                }
-
-                double rotationCorrection = headingError * 0.25 / 45.00;
-                if (rotationCorrection > 0 && rotationCorrection < 0.05) {
-                    rotationCorrection = 0.05;
-                }
-                if (rotationCorrection < 0 && rotationCorrection > -0.05) {
-                    rotationCorrection = -0.05;
-                }
-                // i am trash at coding - mao zendong
-                rotate = rotationCorrection + rotate;
-                telemetry.addData("Fixed Heading", fixedHeading);
-                telemetry.addData("Heading Error", headingError);
-                telemetry.addData("Rotation Correction", rotationCorrection);
-
-            }
-            if (Robot.useCarousel) {
-                if (gamepad2.y) {
-                    driveBrain.carouselMoves();
-                }
-                robot.carousel.setPower(cPower);
-                telemetry.addData("Carousel Power:", cPower);
+            if (Math.abs(headingError) < .5) {
+                headingError = 0;
             }
 
-            robot.setDrive(forward, strafe, rotate, 1);
-            robot.reportEncoders();
-            //robot.reportColor();
+            double rotationCorrection = headingError * 0.25 / 45.00;
+            if (rotationCorrection > 0 && rotationCorrection < 0.05) {
+                rotationCorrection = 0.05;
+            }
+            if (rotationCorrection < 0 && rotationCorrection > -0.05) {
+                rotationCorrection = -0.05;
+            }
 
-            // robotXDrive.calculateDrivePowersFSR(gamepad1.left_stick_x, gamepad1.left_stick_y, rotate);
-            cycles++;
-            //telemetry.addData("cycle time (ms): ", timer.milliseconds() / cycles);
-            telemetry.update();
+            drive_rotate += rotationCorrection;
+            telemetry.addData("Fixed Heading", fixedHeading);
+            telemetry.addData("Heading Error", headingError);
+            telemetry.addData("Rotation Correction", rotationCorrection);
+
         }
+        if (Robot.useCarousel) {
+            if (carousel_cycle) {
+                driveBrain.carouselMoves();
+            }
+            robot.carousel.setPower(carousel_power);
+            telemetry.addData("Carousel Power:", carousel_power);
+        }
+
+        robot.setDrive(drive_forward, drive_strafe, drive_rotate, 1);
+        robot.reportEncoders();
+        //robot.reportColor();
+
+        // robotXDrive.calculateDrivePowersFSR(gamepad1.left_stick_x, gamepad1.left_stick_y, rotate);
+        cycles++;
+        //telemetry.addData("cycle time (ms): ", timer.milliseconds() / cycles);
+        telemetry.update();
     }
 }
