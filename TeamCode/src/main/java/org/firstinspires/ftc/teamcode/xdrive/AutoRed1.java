e70c5a2376339358e282aaece191fcaa8dfed97f
Jackson Taylor <jacksondeantaylor@gmail.com>
2021-12-03 20:20:26 -0600

    removed some status messages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xdrive/AutoRed1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xdrive/AutoRed1.java
@@ -13,8 +13,8 @@ public class AutoRed1 extends LinearOpMode {
     public void runOpMode() {
         brain.init(this);
 
-        telemetry.addData("Status", "Ready to run");    //
-        telemetry.update();
+//        telemetry.addData("Status", "Ready to run");    //
+//        telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
